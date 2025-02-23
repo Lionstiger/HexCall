7fd9dc8c9e53b1f87b0e02921b3b8a21d6c0f230
Magnus Wulf <magnus@wulf.contact>
2025-02-23 22:18:39 +0100

    Setup distributed model plan

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,12 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import AudioConnection from "./hooks/audio_connection"
+import HexPosition from "./hooks/position_update"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: {AudioConnection, HexPosition},
   longPollFallbackMs: 2500,
   params: {_csrf_token: csrfToken}
 })
---
+++ assets/js/hooks/audio_connection.js
@@ -0,0 +1,7 @@
+const AudioCall = {
+    // TODO: actually connect via webrtc to liveview
+    // TODO: request and send audio
+    // TODO: play received audio
+  };
+  
+  export default AudioCall;
