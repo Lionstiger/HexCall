8daefa925d4c4c37da1a966c8e7ffa7d3631f86b
Magnus Wulf <magnus@wulf.contact>
2025-04-28 17:54:45 +0200

    voice signal is working now

--- lib/hexcall_web/live/call_live.ex
+++ lib/hexcall_web/live/call_live.ex
@@ -2,14 +2,45 @@ defmodule HexcallWeb.CallLive do
   use HexcallWeb, :live_view
   require Logger
 
+  alias Membrane.WebRTC.Live.{Capture, Player}
+
+
   @impl true
   def mount(_params, _session, socket) do
     # if connected?(socket) do
     # TODO: setup webrtc connection (both ways) here
     # TODO: sign up to room position topic
     # else
-      {:ok, socket}
+    # {:ok, socket}
     # end
+
+    socket =
+      if connected?(socket) do
+        ingress_signaling = Membrane.WebRTC.Signaling.new()
+        egress_signaling = Membrane.WebRTC.Signaling.new()
+
+        Membrane.Pipeline.start_link(Hexcall.CallPipeline,
+          ingress_signaling: ingress_signaling,
+          egress_signaling: egress_signaling
+        )
+
+        socket
+        |> Capture.attach(
+          id: "mediaCapture",
+          signaling: ingress_signaling,
+          video?: false,
+          audio?: true
+        )
+        |> Player.attach(
+          id: "audioPlayer",
+          signaling: egress_signaling
+        )
+      else
+        socket
+      end
+
+    {:ok, socket}
+
   end
 
   # TODO: setup pipeline to parse uploaded audio
@@ -21,19 +52,29 @@ defmodule HexcallWeb.CallLive do
 
   # TODO: Send Position updates to RoomManager
 
+  # @impl true
+  # def render(assigns) do
+  #   ~H"""
+  #   <div class="call-container">
+  #     <div id="audio-controls" phx-hook="AudioCall" class="p-4">
+  #       <button id="toggle-audio" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+  #         Start Microphone
+  #       </button>
+  #       <div class="mt-2 text-sm text-gray-600">
+  #         Click the button above to start/stop your microphone
+  #       </div>
+  #     </div>
+  #   </div>
+  #   """
+  # end
+
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="call-container">
-      <div id="audio-controls" phx-hook="AudioCall" class="p-4">
-        <button id="toggle-audio" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-          Start Microphone
-        </button>
-        <div class="mt-2 text-sm text-gray-600">
-          Click the button above to start/stop your microphone
-        </div>
-      </div>
-    </div>
+    <h3>Captured stream preview</h3>
+    <Capture.live_render socket={@socket} capture_id="mediaCapture" />
+    <h3>Stream sent by the server</h3>
+    <Player.live_render socket={@socket} player_id="audioPlayer" />
     """
   end
 end
