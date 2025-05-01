c4e2dbfcc6d6e2e758743ff45bfb5c80fd01906d
Magnus Wulf <magnus@wulf.contact>
2025-05-01 20:01:32 +0200

    minor changes to client side js

--- assets/js/hooks/audio_connection.js
+++ assets/js/hooks/audio_connection.js
@@ -1,7 +1,6 @@
 const AudioCall = {
-    // TODO: actually connect via webrtc to liveview
-    // TODO: request and send audio
-    // TODO: play received audio
+// Function to handle the connection to the live view
+// We likely dont need this function as we can directly connect via webRTC in the live view
   };
   
   export default AudioCall;
