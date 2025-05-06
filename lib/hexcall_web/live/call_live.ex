7aeeeffeb382fa167966d1bde102e38eab97bfbd
Magnus Wulf <magnus@wulf.contact>
2025-05-06 17:23:24 +0200

    cleanup and minor changes

--- lib/hexcall_web/live/call_live.ex
+++ lib/hexcall_web/live/call_live.ex
@@ -44,10 +44,10 @@ defmodule HexcallWeb.CallLive do
 
     end
 
-  @impl true
-  def handle_event("webrtc_signaling", data, _socket) do
-    IO.inspect(data)
-  end
+  # @impl true
+  # def handle_event("webrtc_signaling", data, _socket) do
+  #   IO.inspect(data)
+  # end
 
 
   # TODO: setup pipeline to parse uploaded audio
@@ -59,21 +59,6 @@ defmodule HexcallWeb.CallLive do
 
   # TODO: Send Position updates to RoomManager
 
-  # @impl true
-  # def render(assigns) do
-  #   ~H"""
-  #   <div class="call-container">
-  #     <div id="audio-controls" phx-hook="AudioCall" class="p-4">
-  #       <button id="toggle-audio" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-  #         Start Microphone
-  #       </button>
-  #       <div class="mt-2 text-sm text-gray-600">
-  #         Click the button above to start/stop your microphone
-  #       </div>
-  #     </div>
-  #   </div>
-  #   """
-  # end
 
   @impl true
   def render(assigns) do
