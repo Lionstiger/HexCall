dcc84017125f5f4f6cf41d8f10a91069ead3001e
Magnus Wulf <magnus@wulf.contact>
2025-05-27 01:34:46 +0200

    limit hive size to reasonable values(for now)

--- lib/hexcall/hives/hive.ex
+++ lib/hexcall/hives/hive.ex
@@ -6,6 +6,7 @@ defmodule Hexcall.Hives.Hive do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "hives" do
+    # TODO: either make name unique or access by uuid
     field :name, :string
     field :size_x, :integer
     field :size_y, :integer
@@ -19,6 +20,8 @@ defmodule Hexcall.Hives.Hive do
   def changeset(hive, attrs) do
     hive
     |> cast(attrs, [:name, :size_x, :size_y])
+    |> validate_inclusion(:size_x, 3..30)
+    |> validate_inclusion(:size_y, 3..30)
     |> validate_required([:name, :size_x, :size_y])
   end
 end
