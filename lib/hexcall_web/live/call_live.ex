3a5a53a3247a9433f6ee2db374ba21bada07bb6b
Magnus Wulf <magnus@wulf.contact>
2025-05-29 17:27:51 +0200

    basic hive_manager genserver that tracks the users and their positions. gets started dynamically when the first user connects.

--- lib/hexcall_web/live/call_live.ex
+++ lib/hexcall_web/live/call_live.ex
@@ -5,10 +5,15 @@ defmodule HexcallWeb.CallLive do
   # alias Membrane.WebRTC.Live.{Capture, Player}
   alias HexcallWeb.Components.{Capture, Player, HexCell}
   alias Hexcall.Hives
+  alias Hexcall.HiveManager
 
   @impl true
   def mount(%{"hive" => hivename}, _session, socket) do
     hive = Hives.get_hive_by_name_with_hexes(hivename)
+
+    hive_manager = HiveManager.start(hivename)
+    # This needs to be replaced when Auth is added
+    user_id = Ecto.UUID.generate()
 
     if is_nil(hive) do
       {:ok, redirect(socket, to: "/")}
@@ -37,15 +42,29 @@ defmodule HexcallWeb.CallLive do
           socket
         end
 
-      {:ok, socket |> assign(:hive, hive), layout: false}
+      {:ok,
+       socket
+       |> assign(:hive, hive)
+       |> assign(:hive_manager, hive_manager)
+       |> assign(:user_id, user_id), layout: false}
     end
   end
 
   @impl true
   def handle_event("click", data, socket) do
-    IO.inspect(data)
-
+    hive_manager = socket.assigns.hive_manager
+    user_id = socket.assigns.user_id
+    data = Map.delete(data, "value")
+    IO.inspect(HiveManager.move(hive_manager, user_id, data))
+    IO.inspect(HiveManager.get_state(hive_manager))
     {:noreply, socket}
+  end
+
+  @impl true
+  def terminate(_reason, socket) do
+    hive_manager = socket.assigns.hive_manager
+    user_id = socket.assigns.user_id
+    HiveManager.leave(hive_manager, user_id)
   end
 
   # TODO: Setup receiving streams from other. Somehow merge all incoming audio into single output
